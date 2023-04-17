5da2927d8dbf32264f8ba1542c56e114ce599218
Jose Luis Elisseche <joselise61@gmail.com>
2023-04-17 18:27:06 -0300

    Commit desde NetBeans

--- src/mascotaapp/MascotaAPP.java
+++ src/mascotaapp/MascotaAPP.java
@@ -18,21 +18,29 @@ public class MascotaAPP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-       Mascota m1= new Mascota("Pepe");// Hay que poner la misma cantidad que el construcor
        
-//       m1.apodo="Chiquito";
-//       m1.nombre="Coquito";
-//       m1.tipo="Perro";
-//       m1.raza="Beagle";
-//       m1.edad=14;
-
+        Mascota m1= new Mascota("Pepe","Copito","Perro","Blanco",12,true,"Caniche Toy");// Hay que poner la misma cantidad que el construcor
         
        
-       //System.out.println(m1.apodo+ " "+m1.nombre+" "+m1.tipo+ " "+m1.raza+" "+m1.edad+" Años");
+//  m1.apodo="Chiquito";
+//  m1.nombre="Coquito";
+//  m1.tipo="Perro";
+//  m1.raza="Beagle";
+//  m1.edad=14;
+//  System.out.println(m1.apodo+ " "+m1.nombre+" "+m1.tipo+ " "+m1.raza+" "+m1.edad+" Años");
         
-       m1.setNombre("Jose Luis");
+       m1.setNombre("Copito"); // Aca le cambiamos el nombre que originalmente se asigno en la linea 22
+       m1.setTipo("Beagle");
+       System.out.println(m1.toString()); // Antes de aplicar metodo pasear
+       
+       m1.pasear(100);
+              
+        System.out.println(m1.getNombre());
+        System.out.println(m1); // Usando el toString
+        System.out.println(m1.toString()); // Usando el toString igual al anterior
+        
        
     }
+    
     
 }
--- src/mascotaapp/entidades/Mascota.java
+++ src/mascotaapp/entidades/Mascota.java
@@ -18,9 +18,10 @@ public class Mascota {
     private int edad;
     private boolean cola;
     private String raza;
+    private int energia;
 
-    public Mascota(String nombre) {
-    
+    public Mascota() {
+        this.energia=1000;  // asignamos esto 
     }
 
     public Mascota(String nombre, String apodo, String tipo, String color, int edad, boolean cola, String raza) {
@@ -31,12 +32,101 @@ public class Mascota {
         this.edad = edad;
         this.cola = cola;
         this.raza = raza;
+        this.energia=1000;
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        if (nombre.length()>0){
+          this.nombre = nombre;  
+        }
     }
 
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setCola(boolean cola) {
+        this.cola = cola;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+     
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean isCola() {
+        return cola;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    // Metodo de Operacion
+    /**
+     * Funcion destinada a pasear
+     * 
+     * @param energiaRestar
+     * @return energia
+     */
     
+    public int pasear(int energiaRestar){
+        energia=energia - energiaRestar;
+     // energia-= energiaRestar; <= es lo mismo que la linea anterior
+        return energia;
+    }
+    
+    /**
+     * Funcion destinada a pasear con vueltas
+     * 
+     * @param energiaRestar
+     * @param vueltas
+     * @return energia
+     */
+    // Sobrecarga de Metodo de Operacion
+    public int pasear(int energiaRestar, int vueltas){
+        for (int i = 0; i < vueltas; i++) {
+          energia-= energiaRestar;
+       // energia-= energiaRestar; <= es lo mismo que la linea anterior  
+        }
       
+        return energia;
+    }
+    
+    @Override
+    public String toString() {
+        return "Mascota{" + "nombre=" + nombre + ", apodo=" + apodo + ", tipo=" + tipo + ", color=" + color + ", edad=" + edad + ", cola=" + cola + ", raza=" + raza + ", energia=" + energia + '}';
+    }
+    
 }
